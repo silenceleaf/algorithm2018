0e0bc5e81a4bb3fdb832b1f39ae3a8c42ba2d15e
zjunyan <zjunyan@us.ibm.com>
2017-12-29 12:56:05 -0500

    32

--- src/main/java/org/zjy/learn/code/lintcode/MinimumWindowSubstring_32.java
+++ src/main/java/org/zjy/learn/code/lintcode/MinimumWindowSubstring_32.java
@@ -1,14 +1,33 @@
 package org.zjy.learn.code.lintcode;
 
+import org.zjy.learn.util.Application;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * Created by junyan zhang on 12/28/17.
  */
-public class MinimumWindowSubstring_32 {
+
+@Application(time = "12/28/2017 12:40")
+public class MinimumWindowSubstring_32 implements Runnable {
+	private Logger logger = LogManager.getLogManager().getLogger("algorithm_lean");
+
+	@Override
+	public void run() {
+		logger.info("answer: " + minWindow("absdfaabab", "adb"));
+	}
+
+	/**
+	 * wrong answer, count string is not working. such as "aab" and "bba"
+	 * @param source
+	 * @param target
+	 * @return
+	 */
 	public String minWindow(String source , String target) {
 		// write your code here
 		Set<Character> targetHash = new HashSet<>();
@@ -19,29 +38,31 @@ public class MinimumWindowSubstring_32 {
 		int pointer = 0;
 		int start = 0;
 		int begin = 0;
-		int end = 0;
+		int end = Integer.MAX_VALUE;
 		while (start < source.length()) {
-			if (!targetHash.contains(source.charAt(start))) {
-				start++;
-			} else {
+			if (targetHash.contains(source.charAt(start))) {
 				pointer = start;
 				while (pointer < source.length()) {
 					if (targetHash.contains(source.charAt(pointer))) {
 						++get;
 						if (get == target.length()) {
-							if (pointer - start > end - begin) {
+							if (pointer - start < end - begin) {
 								begin = start;
 								end = pointer;
 							}
-							get = 0;
 							break;
 						}
 					}
 					pointer++;
 				}
 			}
+			get = 0;
+			start++;
 		}
-		return source.substring(begin, end + 1);
+		if (end != Integer.MAX_VALUE)
+			return source.substring(begin, end + 1);
+		else
+			return "";
 	}
 
 	public Map<Character, Integer> initHash(String str) {
