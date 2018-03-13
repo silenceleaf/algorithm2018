0554d4f55dbaa2133e2f530e9328f0af5fbacf4d
Junyan Zhang <zjunyan@us.ibm.com>
2018-03-13 18:12:12 -0400

    robot

---
+++ src/main/java/org/zjy/learn/code/google/FindTheCharacter.java
@@ -0,0 +1,15 @@
+package org.zjy.learn.code.google;
+
+import org.zjy.learn.util.Application;
+
+@Application(time = "03/13/2018 13:15")
+public class FindTheCharacter implements Runnable {
+    @Override
+    public void run() {
+
+    }
+
+    public int findInsertCharacter(String s1, String s2) {
+        return 0;
+    }
+}
