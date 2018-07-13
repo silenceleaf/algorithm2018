fac859c870968446a485d6f68492ab426d3652f8
zjunyan <zjunyan@us.ibm.com>
2018-07-13 11:54:52 -0400

    bitsight

--- src/main/java/org/zjy/learn/code/BitSight.java
+++ src/main/java/org/zjy/learn/code/BitSight.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.stream.Stream;
 
 /**
@@ -17,18 +14,19 @@ import java.util.stream.Stream;
 
 public class BitSight  {
 	public static void main(String[]args){
-//		if (args.length < 1) {
-//			System.out.print("Please input file path as the first parameter");
-//			return;
-//		}
-		//String fileName = args[0];
-		String fileName = "/Users/junyan/Downloads/bitsight.txt";
+		if (args.length < 1) {
+			System.out.print("Please input file path as the first parameter");
+			return;
+		}
+		String fileName = args[0];
+		//String fileName = "/Users/junyan/Downloads/bitsight.txt";
 		int n = 0;
 		int m = 0;
 		List<List<Integer>> adjMatrix = new ArrayList<>();
 		List<Integer> indegreeCount = new ArrayList<>();
 		try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(fileName))) {
 			String firstLine = bufferedReader.readLine();
+			// read N and M from first line
 			String[] split = firstLine.split("\\s+");
 			n = Integer.valueOf(split[0]);
 			for (int i = 0; i <= n; i++) {
@@ -36,6 +34,7 @@ public class BitSight  {
 				indegreeCount.add(0);
 			}
 			m = Integer.valueOf(split[1]);
+			// fill the adjacent matrix and indegree count array
 			for (int i = 0; i < m; i++) {
 				String[] line = bufferedReader.readLine().split("\\s+");
 				int target = Integer.valueOf(line[0]);
@@ -45,11 +44,12 @@ public class BitSight  {
 					adjMatrix.get(dependent).add(target);
 				}
 			}
-
+			// main logic: topological sort
 			int[] result = topologicalSort(indegreeCount, adjMatrix);
+			// output the result
 			if (result.length > 0) {
-				for (int i : result) {
-					System.out.print(i);
+				for (int i = 0; i < result.length - 1; i++) {
+					System.out.print(result[i]);
 					System.out.print(" ");
 				}
 			} else {
@@ -62,7 +62,8 @@ public class BitSight  {
 
 	private static int[] topologicalSort(List<Integer> indegreeCount, List<List<Integer>> adjMatrix){
 		int[] order = new int[indegreeCount.size()];
-		Queue<Integer> toVisit = new ArrayDeque<>();
+		Queue<Integer> toVisit = new LinkedList<>();
+		// find the indegree = 0 node, push into queue
 		for (int i = 1; i < indegreeCount.size(); i++) {
 			if (indegreeCount.get(i) == 0)
 				toVisit.offer(i);
@@ -72,12 +73,14 @@ public class BitSight  {
 			int from = toVisit.poll();
 			order[visited++] = from;
 			for (int to : adjMatrix.get(from)) {
+				// remove all edges from this node (indegree = 0)
 				indegreeCount.set(to, indegreeCount.get(to) - 1);
+				// put new indegree = 0 into queue
 				if (indegreeCount.get(to) == 0)
 					toVisit.offer(to);
 			}
 		}
+		// if some node are not visited, means circle exists
 		return visited == indegreeCount.size() - 1 ? order : new int[0];
 	}
-
 }
